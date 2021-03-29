4f1039dcc988bde9cdbd2ef6e8260d76620607d9
Felipe Moura Madureira <44149509+madfelps@users.noreply.github.com>
2021-03-29 20:14:57 -0300

    Delete fanread_arm directory

--- fanread_arm/src/classtorque.h
+++
@@ -1,9 +0,0 @@
-/* Em grandezas compostas de dois bytes temos que o valor referido é a soma do valor do byte menos significativo (LSB) com o byte mais significativo (MSB) multiplicado por 256. (Ver Manual)  */
-
-class Torque:public NegativeValues{
-private:
-
-public:
-	static float ProcessTorque(unsigned char* CAN_DATA, int MSByte, int LSByte);
-
-};
